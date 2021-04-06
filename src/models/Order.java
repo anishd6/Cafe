d35a7197cf10df6b02d6205aa220a04c7c4d7304
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-04-06 10:52:35 -0700

    Added pariwise test case

---
+++ src/models/Order.java
@@ -0,0 +1,270 @@
+package models;
+
+public class Order {
+
+	public int[] orderQtty;
+	public double orderTotal;
+	public String orderReceipt;
+
+	public int orderCalories;
+	public int orderCarbohydrates;
+	public int orderProtein;
+	public int orderFat;
+
+	public int dailyCalories = 1800;
+	public int dailyProtein = 150;
+	public int dailyCarbohydrates = 210;
+	public int dailyFat = 40;
+
+	public Order() {
+		orderQtty = new int[9];
+
+		for (int i = 0; i < 9; i++)
+			orderQtty[i] = 0;
+	}
+
+	public Order(int americano, int cappuccino, int espresso, int hotChocolate, int whiteChocolate, int matchaLatte,
+			int doughnut, int croissant, int cheesecake) {
+		orderQtty = new int[9];
+
+		orderQtty[0] = americano;
+		orderQtty[1] = cappuccino;
+		orderQtty[2] = espresso;
+		orderQtty[3] = hotChocolate;
+		orderQtty[4] = whiteChocolate;
+		orderQtty[5] = matchaLatte;
+		orderQtty[6] = doughnut;
+		orderQtty[7] = croissant;
+		orderQtty[8] = cheesecake;
+
+	}
+
+	public void calculateOrderTotal() {
+
+		int total = 0;
+
+		for (int i = 0; i < orderQtty.length; i++) {
+
+			if (orderQtty[i] != 0) {
+				int itemCount = orderQtty[i];
+
+				switch (i) {
+				case 0:
+					total += itemCount * 3.25;
+					break;
+				case 1:
+					total += itemCount * 3.75;
+					break;
+				case 2:
+					total += itemCount * 3.50;
+					break;
+				case 3:
+					total += itemCount * 3.75;
+					break;
+				case 4:
+					total += itemCount * 3.75;
+					break;
+				case 5:
+					total += itemCount * 4.00;
+					break;
+				case 6:
+					total += itemCount * 1.50;
+					break;
+				case 7:
+					total += itemCount * 2.50;
+					break;
+				case 8:
+					total += itemCount * 4.75;
+					break;
+				}
+
+			}
+
+		}
+
+	}
+
+	public void generateReceipt() {
+		String receipt = "";
+
+		for (int i = 0; i < orderQtty.length; i++) {
+
+			if (orderQtty[i] != 0) {
+				int itemCount = orderQtty[i];
+				switch (i) {
+				case 0:
+					receipt += itemCount + "x Americano \n";
+					break;
+				case 1:
+					receipt += itemCount + "x Capuccino \n";
+					break;
+				case 2:
+					receipt += itemCount + "x Espresso \n";
+					break;
+				case 3:
+					receipt += itemCount + "x Hot Chocolate \n";
+					break;
+				case 4:
+					receipt += itemCount + "x White Chocolate \n";
+					break;
+				case 5:
+					receipt += itemCount + "x Matcha Latte \n";
+					break;
+				case 6:
+					receipt += itemCount + "x Doughnut \n";
+					break;
+				case 7:
+					receipt += itemCount + "x Croissant \n";
+					break;
+				case 8:
+					receipt += itemCount + "x Cheesecake \n";
+					break;
+				}
+			}
+
+		}
+
+		setOrderReceipt(receipt);
+
+	}
+
+	public int[] generateNutrition() {
+
+		int[] orderNutrition = new int[4];
+
+		int calories = 0;
+		int fat = 0;
+		int protein = 0;
+		int carbohydrates = 0;
+
+		for (int i = 0; i < orderQtty.length; i++) {
+
+			if (orderQtty[i] != 0) {
+				int[] itemNutrition = getItemNutriton(i);
+				calories += itemNutrition[0];
+				fat += itemNutrition[1];
+				protein += itemNutrition[2];
+				carbohydrates += itemNutrition[3];
+			}
+
+		}
+
+		orderNutrition[0] = calories;
+		orderNutrition[1] = fat;
+		orderNutrition[2] = protein;
+		orderNutrition[3] = carbohydrates;
+
+		return orderNutrition;
+	}
+
+	public int[] getItemNutriton(int itemCode) {
+
+		if (itemCode < 0 || itemCode > 9)
+			throw new IllegalArgumentException();
+
+		int[] itemNutrition = new int[4];
+
+		int calories = 0;
+		int fat = 0;
+		int protein = 0;
+		int carbohydrates = 0;
+
+		switch (itemCode) {
+		case 0:
+			calories = 15;
+			fat = 0;
+			protein = 1;
+			carbohydrates = 2;
+			break;
+		case 1:
+			calories = 140;
+			fat = 5;
+			protein = 9;
+			carbohydrates = 14;
+			break;
+		case 2:
+			calories = 170;
+			fat = 9;
+			protein = 9;
+			carbohydrates = 14;
+			break;
+		case 3:
+			calories = 440;
+			fat = 19;
+			protein = 15;
+			carbohydrates = 55;
+			break;
+		case 4:
+			calories = 370;
+			fat = 16;
+			protein = 14;
+			carbohydrates = 43;
+			break;
+		case 5:
+			calories = 240;
+			fat = 7;
+			protein = 12;
+			carbohydrates = 34;
+			break;
+		case 6:
+			calories = 480;
+			fat = 27;
+			protein = 5;
+			carbohydrates = 56;
+			break;
+		case 7:
+			calories = 260;
+			fat = 15;
+			protein = 5;
+			carbohydrates = 27;
+			break;
+		case 8:
+			calories = 321;
+			fat = 23;
+			protein = 6;
+			carbohydrates = 26;
+			break;
+		}
+
+		itemNutrition[0] = calories;
+		itemNutrition[1] = fat;
+		itemNutrition[2] = protein;
+		itemNutrition[3] = carbohydrates;
+
+		return itemNutrition;
+
+	}
+
+	public int compareNutrition() {
+		if (orderQtty.length < 0 || orderQtty.length > 9)
+			throw new IllegalArgumentException();
+
+		return orderQtty[0] > dailyCalories || orderQtty[1] > dailyFat || orderQtty[2] > dailyProtein
+				|| orderQtty[3] > dailyCarbohydrates ? 0 : 1;
+	}
+
+	public int[] getOrderQtty() {
+		return orderQtty;
+	}
+
+	public void setOrderQtty(int[] orderQtty) {
+		this.orderQtty = orderQtty;
+	}
+
+	public double getOrderTotal() {
+		return orderTotal;
+	}
+
+	public void setOrderTotal(double orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+
+	public String getOrderReceipt() {
+		return orderReceipt;
+	}
+
+	public void setOrderReceipt(String orderReceipt) {
+		this.orderReceipt = orderReceipt;
+	}
+
+}
---
+++ src/tests/NutritionPWUnit.java
@@ -0,0 +1,66 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import models.Order;
+
+@RunWith(Parameterized.class)
+public class NutritionPWUnit {
+
+	public int[] orderQtty;
+	private int americano;
+	private int cappuccino;
+	private int espresso;
+	private int hotChocolate;
+	private int whiteChocolate;
+	private int matchaLatte;
+	private int doughnut;
+	private int croissant;
+	private int cheesecake;
+
+	@Parameters
+	public static Collection<Object[]> getParameters() {
+		return Arrays.asList(new Object[][] {
+			{1, 0, 0, 1, 0, 0, 1, 0, 0},
+			{0, 1, 0, 0, 1, 0, 1, 0, 0},
+			{0, 0, 1, 0, 0, 1, 1, 0, 0},
+			{0, 0, 1, 0, 1, 0, 0, 1, 0},
+			{0, 1, 0, 1, 0, 0, 0, 1, 0},
+			{1, 0, 0, 0, 0, 1, 0, 1, 0},
+			{1, 0, 0, 0, 1, 0, 0, 0, 1},
+			{0, 1, 0, 0, 0, 1, 0, 0, 1},
+			{0, 0, 1, 1, 0, 0, 0, 0, 1},		
+		});
+	}
+
+	public NutritionPWUnit(int americano, int cappuccino, int espresso, int hotChocolate, int whiteChocolate,
+			int matchaLatte, int doughnut, int croissant, int cheesecake) {
+		this.americano = americano;
+		this.cappuccino = cappuccino;
+		this.espresso = espresso;
+		this.hotChocolate = hotChocolate;
+		this.whiteChocolate = whiteChocolate;
+		this.matchaLatte = matchaLatte;
+		this.doughnut = doughnut;
+		this.croissant = croissant;
+		this.cheesecake = cheesecake;
+	}
+
+	@Test
+//	@Category(SecondaryTest.class)
+	public void testNutritionBalance() {
+		Order order = new Order(americano, cappuccino, espresso, hotChocolate, whiteChocolate, matchaLatte, doughnut,
+				croissant, cheesecake);
+		
+		assertEquals(1, order.compareNutrition());
+	}
+
+}
